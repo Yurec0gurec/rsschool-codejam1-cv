3576d646b9bfe2e446b20731c076c54e804ce256
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-10-07 03:16:15 +0300

    perf: just cleaning the code

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-$(document).ready(function(){
-
-    $("#menu").on("click","a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
+$(document).ready(function () {
+    
+    $("#menu").on("click", "a", function (event) {
         event.preventDefault();
-        //забираем идентификатор блока с атрибута href
-        var id  = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;      
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1000);
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
     });
 });
-
